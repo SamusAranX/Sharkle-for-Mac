d260426b1432f60ed4cbe71ccc765764879f13aa
Peter Wunder <hallo@peterwunder.de>
2017-04-20 01:17:23 +0200

    Initial Commit

---
+++ Sharkle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sharkle
+//
+//  Created by Peter Wunder on 20.04.17.
+//  Copyright © 2017 Peter Wunder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
